85a1f36f2b0a27318450e55fe7d3b75a27a147f9
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-15 21:30:19 +0530

    aa

--- Variables/Outputs/main.tf
+++ Variables/Outputs/main.tf
@@ -2,12 +2,16 @@ variable "ami" {
     default = "ami-02383135f96b2293e"
   
 }
+
 variable "instance_type" {
     default = "t2.micro"
   
 }
 
 resource "aws_instance" "web" {
-  ami = var.ami
-  instance_type=var.instance_type
+    ami = var.ami
+    instance_type   = var.instance_type
+    
+  
 }
+

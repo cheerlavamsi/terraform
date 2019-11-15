7eca70531b6a42b3549d61082fec213714199e88
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-15 19:45:46 +0530

    Added List

---
+++ Variables/List/Main.tf
@@ -0,0 +1,10 @@
+variable "Sample" {
+    type = "list"
+    default = ["Vamsi","Samalya"]
+  }
+resource "null_resource" "SaPmple" {
+  
+  provisioner "local-exec"{
+      command="echo ${var.Sample}"
+  }
+}

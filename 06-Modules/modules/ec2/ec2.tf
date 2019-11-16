3e48a020157dcd629cfbad42f12751826ebbcc69
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-16 08:48:23 +0530

    a

--- 06-Modules/modules/ec2/ec2.tf
+++ 06-Modules/modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
     ami             = var.ami
     instance_type   = var.instance_type
-    tags={
+    tags            = {
     
         Name        = ${var.tags["Project_Name"]}-${var.tags["env"]}
     }

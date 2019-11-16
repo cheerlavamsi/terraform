68743d201bd9e9389028c1112a6d438eb578fbf1
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-16 21:33:25 +0530

    Added new lines

--- Project-01/modules/ec2/ec2.tf
+++ Project-01/modules/ec2/ec2.tf
@@ -6,4 +6,19 @@ resource "aws_instance" "web" {
         Name        = "${var.tags["Project_Name"]}-${var.tags["env"]}"
     } 
 
+provisioner "remote-exec"{
+        connection {
+
+                type        ="ssh"
+                user        ="root"
+                password    ="DevOps321"
+                host        = self.public_ip
+        }
+        inline = [
+
+            "cd/temp",
+            "git clone https://github.com/cheerlavamsi/Shell_Scripts.git",
+            "sh /root/Shell_Scripts/MonolithicServices/webapp.sh ${var.RDS_USERNAME} ${var.RDS_PASSWORD} ${var.RDS_DBNAME} ${var.RDS_ENDPOINT}",
+        ]
+    }
 }
--- Project-01/modules/rds/vars.tf
+++ Project-01/modules/rds/vars.tf
@@ -1,8 +1,11 @@
 variable "RDS_DB_SIZE" {}
 variable "RDS_INSTANCE_TYPE" {}
-variable "RDS_USERNAME" {}
-variable "RDS_PASSWORD" {}
-variable "RDS_DBNAME" {}
 variable "tags" {
   type  =   "map"
 }
+
+variable "RDS_USERNAME" {}
+variable "RDS_PASSWORD" {}
+variable "RDS_DBNAME" {}
+variable "RDS_ENDPOINT" {}
+

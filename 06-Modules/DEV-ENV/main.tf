10975fcb526602891df1999e54b1b1279988c5e4
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-16 08:11:25 +0530

    M2

--- 06-Modules/DEV-ENV/main.tf
+++ 06-Modules/DEV-ENV/main.tf
@@ -1,5 +1,5 @@
 module "DEV-EC2" {
-  source        =   "../../modules/EC2"
+  source        =   "../../modules/ec2"
   ami           =   var.ami
   instance_type =   var.instance_type
   tags          =   var.tags

46271c340d4b905b79b21fabf55068e1edc9b387
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-16 06:10:01 +0530

    Added names

--- Modules/modules/EC2/ec2.tf
+++ Modules/modules/EC2/ec2.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "web" {
-    ami = var.ami
+    ami             = var.ami
     instance_type   = var.instance_type
+    tags={
+        Name        = ${var.tags["Project_Name"]}-${var.tags["env"]}
+    }
      
 }

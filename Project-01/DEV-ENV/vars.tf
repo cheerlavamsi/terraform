bd328b5902543545d59e5ec62330d5112b33c9d9
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-16 20:55:57 +0530

    Added

--- Project-01/DEV-ENV/vars.tf
+++ Project-01/DEV-ENV/vars.tf
@@ -6,7 +6,11 @@ variable "tags" {
 }
 
 variable "RDS_DB_SIZE" {}
-variable "RDS_INSTANCE_TYPE" {
-  
-}
+variable "RDS_INSTANCE_TYPE" {}
+variable "RDS_USERNAME" {}
+variable "RDS_PASSWORD" {}
+variable "RDS_DBNAME" {}
+
+
+
 

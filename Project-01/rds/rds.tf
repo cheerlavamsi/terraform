463880b1bc99b172f2516cba2df520b85f4f1976
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-16 20:51:23 +0530

    Added

--- Project-01/rds/rds.tf
+++ Project-01/rds/rds.tf
@@ -1,12 +1,15 @@
 resource "aws_db_instance" "default" {
     allocated_storage   =   var.RDS_DB_SIZE
     storage_type        =   "gp2"
-    engine              =   "mysql"
-    engine_version      =   "5.7"
+    engine              =   "mariadb"
+    engine_version      =   "10.3"
     instance_class      =   var.RDS_INSTANCE_TYPE
     name                =   var.RDS_DBNAME
     username            =   var.RDS_USERNAME
     password            =   var.RDS_PASSWORD
-    parameter_group_name=   "default.mysql5.7"
+    parameter_group_name=   "default.mariadb10.3"
+    identifier          =   "${var.tags["Project_Name"]}-${var.tags["env"]}- mariadb"
   
 }
+
+

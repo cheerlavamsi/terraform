3f8cc3dd8e8f5892b6110891457e510ccbcd8bd4
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-24 21:50:25 +0530

    aa

--- Project-01/modules/rds/schema.tf
+++ Project-01/modules/rds/schema.tf
@@ -14,7 +14,7 @@ resource "null_resource" "schema" {
   provisioner "local-exec" {
     command = <<EOF
         sudo yum install mariadb -y
-        mysql -h ${aws_db_instance.default.addres} -u${var.RDS_USERNAME} -p${var.RDS_PASSWORD} </tmp/schema.sql
+        mysql -h ${aws_db_instance.default.address} -u${var.RDS_USERNAME} -p${var.RDS_PASSWORD} </tmp/schema.sql
 EOF
   }
 }

72ee91c2ededa8f18674754bcfde2cd862b14670
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-25 19:39:15 +0530

    a

---
+++ Project-01/modules/sg/allow-web-public.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "allow-web-public" {
+  name        = "allow_web_public"
+  description = "Allow WEB Access for PUBLIC"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+output "SG_WEB_PUB" {
+  value = aws_security_group.allow-web-public.id
+}

089b54f4e5eded6f0d582b34887cd784457efa91
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-26 08:58:28 +0530

    aa

--- Project-01/DEV-ENV/provider.tf
+++ Project-01/DEV-ENV/provider.tf
@@ -4,3 +4,11 @@ provider "aws" {
   #region  = "us-east-1"
   region = "us-west-2"
 }
+
+terrform {
+  backend "s3" {
+    bucket  = "d42-student"
+    key     = "terraform-remote-states/proj-01/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}

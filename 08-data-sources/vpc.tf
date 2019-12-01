efae1ca9860d9d0d750eee49d7be38e6182fe9b1
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-01 14:17:42 +0530

    add

--- 08-data-sources/vpc.tf
+++ 08-data-sources/vpc.tf
@@ -1,5 +1,23 @@
 data "aws_vpcs" "foo" {}
 
+locals {
+  vpcs = tolist(data.aws_vpcs.foo.ids)
+}
+
+data "aws_vpc" "foo" {
+  count = length(local.vpcs)
+  id    = element(local.vpcs, count.index)
+}
+
+variable "new" {
+  type = "list"
+  default = ["a", "b"]
+}
+
 output "foo" {
-  value = data.aws_vpcs.foo.ids
+  value = length(data.aws_vpc.foo.*.arn)
+}
+
+output "new" {
+  value = var.new[0]
 }

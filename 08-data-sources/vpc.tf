b3a8c3f2b862e217744530e8fbc816f850e85b22
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-01 15:01:48 +0530

    add

--- 08-data-sources/vpc.tf
+++ 08-data-sources/vpc.tf
@@ -1,9 +1,29 @@
+data "aws_vpcs" "foo" {}
+
 resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
   }
 }
 
-output "vpc" {
-  value = aws_default_vpc.default.id
+locals {
+  vpcs = tolist(data.aws_vpcs.foo.ids)
+}
+
+data "aws_vpc" "foo" {
+  count = length(local.vpcs)
+  id    = element(local.vpcs, count.index)
+}
+
+variable "new" {
+  type = "list"
+  default = ["a", "b"]
+}
+
+output "foo" {
+  value = length(data.aws_vpc.foo.*.arn)
+}
+
+output "new" {
+  value = var.new[0]
 }

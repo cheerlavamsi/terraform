84ec2fcf8a2cbfbebb22fc95be61da710f2ec524
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-01 15:16:46 +0530

    add

---
+++ 09-rmote-state-locking/dynamodb.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "sleep 120"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "d42-student"
+    key    = "terraform-remote-states/sample-locking/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locking"
+  }
+}

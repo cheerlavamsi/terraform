f929372b470c586b0687aeb6a170940ee19035f6
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-02 20:20:59 +0530

    add

--- Project-01/DEV-ENV/provider.tf
+++ Project-01/DEV-ENV/provider.tf
@@ -8,7 +8,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket  = "d42-student"
-    key     = "terraform-remote-states/proj-01/terraform.tfstate"
+    key     = "terraform-remote-states/sample-locking/terraform.tfstate"
     region  = "us-east-1"
     dynamodb_table = "terraform-up-and-running-locks"
   }

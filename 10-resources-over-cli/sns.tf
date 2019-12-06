7cf19c950014affe69b0a12c666fd17a40ee5927
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-06 06:25:48 +0530

    add

--- 10-resources-over-cli/sns.tf
+++ 10-resources-over-cli/sns.tf
@@ -3,9 +3,6 @@ resource "null_resource" "create-sns" {
         command     =   "aws sns create-topic --name sample"
     }
 
-    provisioner "local-exec"{
-        when        =   "destroy"
-        command     =   "aws sns delete-topic --topic-arn arn:aws:sns:us-east-1:130241414257:sample"
-    }
+ 
   
 }

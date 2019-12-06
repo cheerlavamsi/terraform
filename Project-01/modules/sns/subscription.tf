0b27a2968aa34426167355cd0fe636e588e6cdcf
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-06 08:31:29 +0530

    add

--- Project-01/modules/sns/subscription.tf
+++ Project-01/modules/sns/subscription.tf
@@ -2,11 +2,12 @@ resource "null_resource" "create-email-subscription" {
     count   =   length(var.SNS_SUBSCRIPTION_EMAIL)
 
   provisioner "local-exec" {
-    command = "aws sns subcribe --topic-arn ${aws_sns_topic.monitoring-emails-notify.arn} --protocol email --notification-endpoint ${element(var.SNS_SUBSCRIPTION_EMAIL, count.index)}"
+    command = "aws sns subscribe --topic-arn ${aws_sns_topic.monitoring-emails-notify.arn} --protocol email --notification-endpoint ${element(var.SNS_SUBSCRIPTION_EMAIL, count.index)}"
   }
 
   provisioner "local-exec" {
     when = "destroy"
     command = "aws sns unsubscribe --subscription-arn"
+    
   }
 }

182a72b77f894483b338300e287be7700aab6149
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-18 13:53:06 +0530

    add

--- Project-01/modules/VPC/subnets.tf
+++ Project-01/modules/VPC/subnets.tf
@@ -9,6 +9,17 @@ resource "aws_subnet" "public-subnets" {
   }
 }
 
+resource "aws_subnet" "private-subnets" {
+  count                   = length(data.aws_availability_zones.available.zone_ids)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.VPC_CIDR, 8, count.index+length(data.aws_availability_zones.available.zone_ids))
+
+  tags = {
+    Name                  = "${var.tags["project_name"]}-${var.tags["env"]}-private-subnet-${count.index+1}"
+  }
+}
+
+
 resource "aws_route_table_association" "a" {
   count                   = length(data.aws_availability_zones.available.zone_ids)
   subnet_id               = element(aws_subnet.public-subnets.*.id, count.index )

0cb60bf7b7d70a5ab03438e659ac23786ba5bb78
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-25 15:36:12 +0530

    add

--- Project-01/TEST-ENV/provider.tf
+++ Project-01/TEST-ENV/provider.tf
@@ -1,14 +1,7 @@
-provider "aws" {
-  alias  = "oregon"
-  #version = "~> 2.0"
-  #region  = "us-east-1"
-  region = "us-west-2"
-}
-
 terraform {
   backend "s3" {
     bucket          = "d42-student"
-    key             = "terraform-remote-states/proj-01/terraform.tfstate"
+    key             = "terraform-remote-states/proj-01/test/terraform.tfstate"
     region          = "us-east-1"
     dynamodb_table  = "terraform-locking"
     #dynamodb_table = "terraform-up-and-running-locks"
